c141be307b91a21ecb8dae6e9856c84ef9d1b789
Ngo Manh Truong-LB <ngomanhtruong123@gmail.com>
2023-11-28 21:14:05 +0700

    x

--- controllers/VideoAi/youtube.js
+++ controllers/VideoAi/youtube.js
@@ -13,271 +13,293 @@ const SCOPES = ["https://www.googleapis.com/auth/youtube.upload"];
 var TOKEN_DIR = process.env.storage_tv365 + "/video/videoai";
 const TOKEN_PATH = TOKEN_DIR + "/token.json";
 const TOKEN_PATH_TV = TOKEN_DIR + "/token_tv.json";
-
+const tw = require("./twitter");
+const fb = require("./facebook");
 // type = 1 work247 , 2: timviec
 
 const categoryIds = {
-  Entertainment: 24,
-  Education: 27,
-  ScienceTechnology: 28,
+    Entertainment: 24,
+    Education: 27,
+    ScienceTechnology: 28,
 };
 const credentialsObject = {
-  web: {
-    client_id:
-      "107099070730-flcu1l4632rjdpr0relqlv0j33qd5nd5.apps.googleusercontent.com",
-    project_id: "update-youtube-405003",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_secret: "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",
-    redirect_uris: [
-      "http://localhost:3000/getToken",
-      "http://localhost:3000/getTokenWork",
-    ],
-    javascript_origins: [
-      "http://localhost",
-      "http://localhost:5000",
-      "https://update--405003.firebaseapp.com",
-    ],
-  },
+    web: {
+        client_id:
+            "981724264980-mp7mgvt16sv8s266lrvseqspifae2umm.apps.googleusercontent.com",
+        project_id: "dazzling-reach-406506",
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url:
+            "https://www.googleapis.com/oauth2/v1/certs",
+        client_secret: "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",
+        redirect_uris: [
+            "https://hungha365.com/video-ai/getToken",
+            "https://hungha365.com/video-ai/getTokenWork",
+        ],
+    },
 };
 
 exports.getTokenYoutube = async (req, res, next) => {
-  let type = req.body.type;
-  let clientSecret = credentialsObject.web.client_secret;
-  let clientId = credentialsObject.web.client_id;
-  let redirectUrl = "";
-  if (type == 2) {
-    redirectUrl = credentialsObject.web.redirect_uris[0];
-  } else if (type == 1) {
-    redirectUrl = credentialsObject.web.redirect_uris[1];
-  }
-  var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
-  const authUrl = oauth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: SCOPES,
-  });
-  res.status(200).send({
-    data: {
-      url: authUrl,
-    },
-  });
-};
-function getNewToken(oauth2Client, code, type) {
-  oauth2Client.getToken(code, function (err, token) {
-    if (err) {
-      console.log("Error while trying to retrieve access token", err);
-      return;
-    }
-    oauth2Client.credentials = token;
-    storeToken(token, type);
-  });
-}
-function storeToken(token, type) {
-  try {
-    fs.mkdirSync(TOKEN_DIR);
-  } catch (err) {
-    if (err.code != "EEXIST") {
-      return null;
-    }
-  }
-  console.log(JSON.stringify(token));
-  console.log(type);
-  if (type == 2) {
-    fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-      if (err) throw err;
-      console.log("Token stored to " + TOKEN_PATH);
-    });
-  } else if (type == 1) {
-    fs.writeFile(TOKEN_PATH_TV, JSON.stringify(token), (err) => {
-      if (err) throw err;
-      console.log("Token stored to " + TOKEN_PATH_TV);
-    });
-  }
-}
-function authorize(credentials, callback) {
-  var clientSecret = credentials.web.client_secret;
-  var clientId = credentials.web.client_id;
-  var redirectUrl = credentials.web.redirect_uris[0];
-  var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function (err, token) {
-    console.log(token);
-    oauth2Client.credentials = JSON.parse(token);
-    callback(oauth2Client);
-  });
-}
-
-exports.uploadYoutube = async (req, res) => {
-  try {
-    let { id_blog, type, com_name, title, description } = req.body;
-    console.log(id_blog, type, com_name, title, description);
-
-    let resp = await VideoAi.findOne({
-      id_blog: id_blog,
-      type: type,
-      com_name: com_name,
-    });
-    console.log(resp);
-    if (resp) {
-      authorize(credentialsObject, async (auth) => {
-        await getChannel(
-          resp.link_server,
-          auth,
-          title,
-          description,
-          resp.id,
-          res
-        );
-      });
-    } else {
-      return functions.setError(res, {
-        message: "Couldn't find id_blog",
-      });
-    }
-  } catch (err) {
-    return functions.setError(res, error.message);
-  }
-};
-exports.updateTokenYoutube = async (req, res, next) => {
-  try {
-    const { code, type } = req.body;
+    let type = req.body.type;
     let clientSecret = credentialsObject.web.client_secret;
     let clientId = credentialsObject.web.client_id;
     let redirectUrl = "";
     if (type == 2) {
-      redirectUrl = credentialsObject.web.redirect_uris[0];
+        redirectUrl = credentialsObject.web.redirect_uris[0];
     } else if (type == 1) {
-      redirectUrl = credentialsObject.web.redirect_uris[1];
+        redirectUrl = credentialsObject.web.redirect_uris[1];
     }
-    console.log(JSON.stringify(code));
-
     var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
-    await getNewToken(oauth2Client, code, type);
-    res.status(200).send({
-      data: {
-        result: true,
-      },
+    const authUrl = oauth2Client.generateAuthUrl({
+        access_type: "offline",
+        scope: SCOPES,
     });
-  } catch (err) {
-    return functions.setError(res, err.message);
-  }
+    res.status(200).send({
+        data: {
+            url: authUrl,
+        },
+    });
 };
-async function getChannel(
-  videoFilePath,
-  auth,
-  title,
-  description,
-  id,
-  com_name
-) {
-  var service = google.youtube("v3");
-  service.videos.insert(
-    {
-      auth: auth,
-      part: "snippet,status",
-      requestBody: {
-        snippet: {
-          title,
-          description,
-          categoryId: categoryIds.ScienceTechnology,
-          defaultLanguage: "vi",
-          defaultAudioLanguage: "vi",
-        },
-        status: {
-          privacyStatus: "public",
-        },
-      },
-      media: {
-        body: fs.createReadStream(videoFilePath),
-      },
-    },
-    // function (err, response) {
-    //     if (err) {
-    //         console.log("The API returned an error: " + err);
-    //         return;
-    //     }
-    //     console.log(response.data);
-
-    //     console.log("Video uploaded. Uploading the thumbnail now.");
-    //     // service.thumbnails.set(
-    //     //     {
-    //     //         auth: auth,
-    //     //         videoId: response.data.id,
-    //     //         media: {
-    //     //             body: fs.createReadStream(thumbFilePath),
-    //     //         },
-    //     //     },
-    //     //     function (err, response) {
-    //     //         if (err) {
-    //     //             console.log("The API returned an error: " + err);
-    //     //             return;
-    //     //         }
-    //     //         console.log(response.data);
-    //     //     }
-    //     // );
-    // }
-    async function (err, response) {
-      if (err) {
-        console.log("The API returned an error: " + err);
-      }
-      console.log(response.data);
-      const videoId = response.data.id;
-      console.log("uploading video " + videoId);
-      const videoLink = `https://www.youtube.com/watch?v=${videoId}`;
-      await VideoAi.updateOne(
-        { id: id },
-        {
-          link_youtube: videoLink,
-          id_youtube: videoId,
-          status_server: 1,
-          link_server: "",
+function getNewToken(oauth2Client, code, type) {
+    oauth2Client.getToken(code, function (err, token) {
+        if (err) {
+            console.log("Error while trying to retrieve access token", err);
+            return;
         }
-      );
-      fs.unlink(videoFilePath, (err) => {
-        if (err) return null;
-      });
-      let tweet_content = {
-        text: `${title} Link-bai-viet: ${videoLink}`,
-      };
-      if (com_name == "work247") {
-        await tweet(1, id, tweet_content);
-      } else if (com_name == "timviec365") {
-        await tweet(2, id, tweet_content);
-      }
+        oauth2Client.credentials = token;
+        storeToken(token, type);
+    });
+}
+function storeToken(token, type) {
+    try {
+        fs.mkdirSync(TOKEN_DIR);
+    } catch (err) {
+        if (err.code != "EEXIST") {
+            return null;
+        }
     }
-  );
+    console.log(JSON.stringify(token));
+    console.log(type);
+    if (type == 2) {
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+            if (err) throw err;
+            console.log("Token stored to " + TOKEN_PATH);
+        });
+    } else if (type == 1) {
+        fs.writeFile(TOKEN_PATH_TV, JSON.stringify(token), (err) => {
+            if (err) throw err;
+            console.log("Token stored to " + TOKEN_PATH_TV);
+        });
+    }
+}
+function authorize(credentials, callback) {
+    var clientSecret = credentials.web.client_secret;
+    var clientId = credentials.web.client_id;
+    var redirectUrl = credentials.web.redirect_uris[0];
+    var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, function (err, token) {
+        console.log(token);
+        oauth2Client.credentials = JSON.parse(token);
+        callback(oauth2Client);
+    });
 }
 
-async function filterVideoAi(data, index) {
-  if (index < data.length) {
-    await authorize(credentialsObject, async (auth) => {
-      await getChannel(
-        data[index].link_server,
-        auth,
-        data[index].title,
-        data[index].description,
-        data[index].id,
-        data[index].com_name
-      );
-    });
-    // setTimeout(() => {
-    //   filterVideoAi(data, index + 1);
-    // }, 10000);
-  }
+exports.uploadYoutube = async (req, res) => {
+    try {
+        let { id_blog, type, com_name, title, description } = req.body;
+        console.log(id_blog, type, com_name, title, description);
+
+        let resp = await VideoAi.findOne({
+            id_blog: id_blog,
+            type: type,
+            com_name: com_name,
+        });
+        console.log(resp);
+        if (resp) {
+            authorize(credentialsObject, async (auth) => {
+                await getChannel(
+                    resp.link_server,
+                    auth,
+                    title,
+                    description,
+                    resp.id,
+                    res
+                );
+            });
+        } else {
+            return functions.setError(res, {
+                message: "Couldn't find id_blog",
+            });
+        }
+    } catch (err) {
+        return functions.setError(res, error.message);
+    }
+};
+exports.updateTokenYoutube = async (req, res, next) => {
+    try {
+        const { code, type } = req.body;
+        let clientSecret = credentialsObject.web.client_secret;
+        let clientId = credentialsObject.web.client_id;
+        let redirectUrl = "";
+        if (type == 2) {
+            redirectUrl = credentialsObject.web.redirect_uris[0];
+        } else if (type == 1) {
+            redirectUrl = credentialsObject.web.redirect_uris[1];
+        }
+        console.log(JSON.stringify(code));
+
+        var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
+        await getNewToken(oauth2Client, code, type);
+        res.status(200).send({
+            data: {
+                result: true,
+            },
+        });
+    } catch (err) {
+        return functions.setError(res, err.message);
+    }
+};
+
+async function authorizeTest(credentials) {
+    let clientSecret = credentials.web.client_secret;
+    let clientId = credentials.web.client_id;
+    let redirectUrl = credentials.web.redirect_uris[0];
+    let oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
+    let tokenInit = null;
+    try {
+        const token = await fs.readFileSync(TOKEN_PATH);
+        tokenInit = JSON.parse(token);
+        oauth2Client.credentials = tokenInit;
+    } catch (err) {
+        console.error("Error reading token file:", err);
+    }
+
+    return oauth2Client;
+}
+async function getChannel(
+    videoFilePath,
+    auth,
+    title,
+    description,
+    id,
+    com_name
+) {
+    var service = google.youtube("v3");
+
+    service.videos.insert(
+        {
+            auth: auth,
+            part: "snippet,status",
+            requestBody: {
+                snippet: {
+                    title,
+                    description,
+                    categoryId: categoryIds.ScienceTechnology,
+                    defaultLanguage: "vi",
+                    defaultAudioLanguage: "vi",
+                },
+                status: {
+                    privacyStatus: "public",
+                },
+            },
+            media: {
+                body: fs.createReadStream(videoFilePath),
+            },
+        },
+        // function (err, response) {
+        //     if (err) {
+        //         console.log("The API returned an error: " + err);
+        //         return;
+        //     }
+        //     console.log(response.data);
+
+        //     console.log("Video uploaded. Uploading the thumbnail now.");
+        //     // service.thumbnails.set(
+        //     //     {
+        //     //         auth: auth,
+        //     //         videoId: response.data.id,
+        //     //         media: {
+        //     //             body: fs.createReadStream(thumbFilePath),
+        //     //         },
+        //     //     },
+        //     //     function (err, response) {
+        //     //         if (err) {
+        //     //             console.log("The API returned an error: " + err);
+        //     //             return;
+        //     //         }
+        //     //         console.log(response.data);
+        //     //     }
+        //     // );
+        // }
+        async function (err, response) {
+            if (err) {
+                console.log("The API returned an error: " + err);
+            }
+            console.log(response.data);
+            const videoId = response.data.id;
+            console.log("uploading video " + videoId);
+            const videoLink = `https://www.youtube.com/watch?v=${videoId}`;
+            await VideoAi.updateOne(
+                { id: id },
+                {
+                    link_youtube: videoLink,
+                    id_youtube: videoId,
+                    status_server: 1,
+                    link_server: "",
+                }
+            );
+            fs.unlink(videoFilePath, (err) => {
+                if (err) return null;
+            });
+            let tweet_content = {
+                text: `${title} Link-bai-viet: ${videoLink}`,
+            };
+            await tw.tweet(com_name, id, tweet_content);
+            await fb.postFb(com_name, title, videoLink, id);
+        }
+    );
+}
+
+async function filterVideoAi(data, res, auth, index) {
+    if (index < 5) {
+        // await authorize(credentialsObject, async (auth) => {
+        //     await getChannel(
+        //         data[index].link_server,
+        //         auth,
+        //         data[index].title,
+        //         data[index].description,
+        //         data[index].id,
+        //         data[index].com_name
+        //     );
+        // });
+        // console.log(data[index]);
+        await getChannel(
+            data[index].link_server,
+            auth,
+            data[index].title,
+            data[index].description,
+            data[index].id,
+            data[index].com_name
+        );
+        setTimeout(() => {
+            filterVideoAi(data, res, auth, index + 1);
+        }, 30000);
+    }
 }
 exports.run = async (req, res, next) => {
-  try {
-    let { com_name } = req.body;
-    let data = await VideoAi.find({
-      com_name: com_name,
-      status_server: 0,
-    });
-    await filterVideoAi(data, 0);
-    res.status(200).send({
-      data: data,
-    });
-  } catch (err) {
-    return functions.setError(res, err.message);
-  }
+    try {
+        let { com_name } = req.body;
+        let data = await VideoAi.find({
+            com_name: com_name,
+            status_server: 0,
+        });
+        let auth = await authorizeTest(credentialsObject);
+        filterVideoAi(data, res, auth, 0);
+        res.status(200).send({
+            message: "ok",
+        });
+    } catch (err) {
+        return functions.setError(res, err.message);
+    }
 };
--- routes/VideoAi/UpdateFile.js
+++ routes/VideoAi/UpdateFile.js
@@ -2,8 +2,9 @@ const router = require("express").Router();
 const formData = require("express-form-data");
 const controller = require("../../controllers/VideoAi/UpdateVideo");
 const tiktok = require("../../controllers/VideoAi/tiktok");
-const twitter = require("../../controllers/VideoAi/twitter");
+const tw = require("../../controllers/VideoAi/twitter");
 const youtube = require("../../controllers/VideoAi/youtube");
+const fb = require("../../controllers/VideoAi/facebook");
 // main
 router.post("/getListBlogWork247", controller.getListBlogWork247);
 router.post("/getListBlogTimViec", controller.getListBlogTimViec);
@@ -21,8 +22,13 @@ router.post("/run", youtube.run);
 
 // twitter
 // router.post("/twitter_v2_self_owned", twitter.twitter_v2_self_owned);
-// router.post("/twitter_tweet", twitter.twitter_tweet);
+router.post("/twitter_tweet", tw.twitter_tweet);
 
 // tiktok
 // router.post("/getTokenTikTok", tiktok.getTokenTikTok);
+
+// facebook
+router.post("/getTokenFacebook", fb.getTokenFacebook);
+router.post("/postNewFacebook", fb.postNewFacebook);
+
 module.exports = router;
